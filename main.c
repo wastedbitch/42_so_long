7af4eec99151dad75f33410a4ee8f3816fe4a8c9
fish <wastedbxtch@gmail.com>
2025-03-09 16:36:46 +0100

    rendering png to bg meow

--- main.c
+++ main.c
@@ -3,119 +3,58 @@
 // See README in the root project for more information.
 // -----------------------------------------------------------------------------
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <MLX42/MLX42.h>
+#include <stdio.h>
+#include "MLX42/MLX42.h"
 
-#define WIDTH 1920
-#define HEIGHT 1080
+#define WIDTH 420
+#define HEIGHT 420
 
-static mlx_image_t* image;
-
-// -----------------------------------------------------------------------------
-
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+static void error(void)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+  puts(mlx_strerror(mlx_errno));
+  exit(EXIT_FAILURE);
 }
-
-void ft_randomize(void* param)
-{
-	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
-	{
-		for (uint32_t y = 0; y < image->height; ++y)
-		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
-			mlx_put_pixel(image, i, y, color);
-		}
-	}
-}
-
-void ft_hook(void* param)
-{
-	mlx_t* mlx = param;
-
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
-}
-
-// -----------------------------------------------------------------------------
 
 int32_t main(void)
 {
-	mlx_t* mlx;
+	// Start mlx
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Color Game", false);
+	if (!mlx)
+		error();
 
-	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+  // Try to load the file
+	mlx_texture_t* texture = mlx_load_png("./ass/42x42_bg1.png");
+	if (!texture)
+		error();
+  
+  // Convert texture to a displayable image
+
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+		error();
+
+  // Display the image
+	int x = 0;
+	int y = 0;
+	while (x < 10)
 	{
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		y = 0;
+		while (y < 10)
+		{
+			if (mlx_image_to_window(mlx, img, x*42, y*42) < 0)
+				error();
+			y++;
+		}
+		x++;
 	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
-	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
 
 	mlx_loop(mlx);
+
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+	
+	// Optional, terminate will clean up any leftover images (not textures!)
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
-
-// #include "so_long.h"
-
-// int32_t main(void)
-// {
-// 	mlx_t* mlx;
-
-// 	// Gotta error check this stuff
-// 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-// 	{
-// 		puts(mlx_strerror(mlx_errno));
-// 		return(EXIT_FAILURE);
-// 	}
-// 	if (!(image = mlx_new_image(mlx, 128, 128)))
-// 	{
-// 		mlx_close_window(mlx);
-// 		puts(mlx_strerror(mlx_errno));
-// 		return(EXIT_FAILURE);
-// 	}
-// 	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-// 	{
-// 		mlx_close_window(mlx);
-// 		puts(mlx_strerror(mlx_errno));
-// 		return(EXIT_FAILURE);
-// 	}
-	
-// 	mlx_loop_hook(mlx, ft_randomize, mlx);
-// 	mlx_loop_hook(mlx, ft_hook, mlx);
-
-// 	mlx_loop(mlx);
-// 	mlx_terminate(mlx);
-// 	return (EXIT_SUCCESS);
-// }
